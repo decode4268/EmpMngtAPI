574a29f3573204c9945f25385ac32fef978cbe72
Divyanshu Rai <raidivyanshu19032000@gmail.com>
2026-06-10 08:49:37 +0530

    09-Jun-2026

---
+++ EmpMngtAPI/Controllers/JobDetailsController.cs
@@ -0,0 +1,33 @@
+﻿using EmpMngtAPI.DataModel;
+using EmpMngtAPI.Model.ResponseModel;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EmpMngtAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class JobDetailsController : ControllerBase
+    {
+        private readonly AppDbContext _context;
+
+        public JobDetailsController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet("GetJobLocation")]
+        public IActionResult GetJobLocation()
+        {
+            var jobLocations = _context.locationTbls
+                .Select(l => new JobLocationDTO
+                {
+                    Id = l.Id,
+                    Value = l.Value,
+                    Description = l.Description ?? "NA"
+                }).Distinct()
+                .ToList();
+            return Ok(jobLocations);
+        }
+    }
+}
---
+++ EmpMngtAPI/Model/ResponseModel/JobLocationDTO.cs
@@ -0,0 +1,9 @@
+﻿namespace EmpMngtAPI.Model.ResponseModel
+{
+    public class JobLocationDTO
+    {
+        public int Id { get; set; }
+        public string Value { get; set; }
+        public string Description { get; set; }
+    }
+}
